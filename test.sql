df14b98c7b8bcbeec22202d79a2904a4faf2dc9b
Andrew Reed <andrew.reed.r@gmail.com>
2022-08-31 13:00:32 -0400

    create table statements

---
+++ test.sql
@@ -0,0 +1,12 @@
+create table users (
+    id serial primary key,
+    name varchar(100),
+    email text unique not null,
+    entries bigint default 0,
+    joined timestamp not null
+);
+create table login (
+    id serial primary key,
+    hash varchar(100),
+    email text unique not null
+);
